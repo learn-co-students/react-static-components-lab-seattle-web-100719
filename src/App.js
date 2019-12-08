ff7d27eccbd9cab04d502c26809f8b57fca6e9b6
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2019-12-07 21:32:31 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
+
 import React, { Component } from 'react';
+import CatComponent from './CatComponent'
+import EinsteinQuoteComponent from './EinsteinQuoteComponent'
+import MouseComponent from './MouseComponent'
 
 class App extends Component {
 	render() {
@@ -7,7 +11,7 @@ class App extends Component {
 			<div className="App">
 				<CatComponent />
 				<EinsteinQuoteComponent />
-				{/* one more component missing */}
+				<MouseComponent />
 			</div>
 		);
 	}
